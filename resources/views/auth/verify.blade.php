716f6cae8a11e52b9d864efc3d03217e5ce8e70b
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2022-07-23 22:53:00 +0700

    edit responsive app #5

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <script>
-        window.location.href = "{{ route('profile') }}";
+        window.location.href = "{{ route('profil') }}";
     </script>
     <div class="container">
         <div class="row justify-content-center">
